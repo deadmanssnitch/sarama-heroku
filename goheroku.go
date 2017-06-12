cce5c71025bfeea09a50c8fc1cf18d874cc72d1a
Meredith Lind <meredith.lind@hope.edu>
2017-06-12 11:39:28 -0400

    Refactor appConfig and funs accordingly
    The app config is no longer public and now holds the broker addresses
    and the tls config. This required us to do some restructing of
    functions.

--- goheroku.go
+++ goheroku.go
@@ -16,59 +16,65 @@ import (
 //
 //
 
-type AppConfig struct {
+type appConfig struct {
 	URL           string `env:"KAFKA_URL,required"`
 	TrustedCert   string `env:"KAFKA_TRUSTED_CERT,required"`
 	ClientCertKey string `env:"KAFKA_CLIENT_CERT_KEY,required"`
 	ClientCert    string `env:"KAFKA_CLIENT_CERT,required"`
 	Prefix        string `env:"KAFKA_PREFIX"`
+	TLSConfig     *tls.Config
+	BrokerAddrs   []string
 }
 
 // <<Some helpful documentation here>>
 //
 func NewConsumer(topic string, consumerGroup string) (*cluster.Consumer, error) {
-	ac, _ := setupConnection
-	consumer, err := ac.createKafkaConsumer(brokerAddrs, tlsConfig)
+	ac, _ := setupConnection()
+	consumer, err := ac.createKafkaConsumer(topic, consumerGroup, ac.BrokerAddrs, ac.TLSConfig)
 	if err != nil {
 		return nil, err
 	}
+	return consumer, nil
 }
 
-func NewAsyncProducer() (*sarama.AsyncProducer, error) {
-	ac, _ := setupConnection
-	producer, err := ac.createKafkaAsyncProducer(brokerAddrs, tlsConfig)
+func NewAsyncProducer() (sarama.AsyncProducer, error) {
+	ac, _ := setupConnection()
+	producer, err := ac.createKafkaAsyncProducer(ac.BrokerAddrs, ac.TLSConfig)
 	if err != nil {
 		return nil, err
 	}
+	return producer, nil
 }
 
-func NewSyncProducer() (*sarama.SyncProducer, error) {
-	ac, _ := setupConnection
-	producer, err := ac.createKafkaSyncProducer(brokerAddrs, tlsConfig)
+func NewSyncProducer() (sarama.SyncProducer, error) {
+	ac, _ := setupConnection()
+	producer, err := ac.createKafkaSyncProducer(ac.BrokerAddrs, ac.TLSConfig)
 	if err != nil {
 		return nil, err
 	}
+	return producer, nil
 }
 
-func setupConnection() (*AppConfig, error) {
-	ac := AppConfig{}
+func setupConnection() (*appConfig, error) {
+	ac := appConfig{}
 	err := envdecode.Decode(&ac)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
-	tlsConfig, err := ac.createTLSConfig()
+	err = ac.createTLSConfig()
 	if err != nil {
 		return nil, err
 	}
-	brokerAddrs, err := ac.brokerAddresses()
+	err = ac.brokerAddresses()
 	if err != nil {
 		return nil, err
 	}
+	return &ac, nil
 }
 
 // Create the TLS context, using the key and certificates provided.
-func (ac *AppConfig) createTLSConfig() (*tls.Config, error) {
+func (ac *appConfig) createTLSConfig() error {
 	roots := x509.NewCertPool()
 	ok := roots.AppendCertsFromPEM([]byte(ac.TrustedCert))
 	if !ok {
@@ -77,50 +83,51 @@ func (ac *AppConfig) createTLSConfig() (*tls.Config, error) {
 	// Setup certs for Sarama
 	cert, err := tls.X509KeyPair([]byte(ac.ClientCert), []byte(ac.ClientCertKey))
 	if err != nil {
-		return nil, err
+		return err
 	}
-	tlsConfig := &tls.Config{
+	ac.TLSConfig = &tls.Config{
 		Certificates:       []tls.Certificate{cert},
 		InsecureSkipVerify: true,
 		RootCAs:            roots,
 	}
-	return tlsConfig, nil
+	return nil
 }
 
 // Extract the host:port pairs from the Kafka URL(s)
-func (ac *AppConfig) brokerAddresses() ([]string, error) {
+func (ac *appConfig) brokerAddresses() error {
 	urls := strings.Split(ac.URL, ",")
 	addrs := make([]string, len(urls))
 	for i, v := range urls {
 		u, err := url.Parse(v)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		addrs[i] = u.Host
+		ac.BrokerAddrs = addrs
 	}
-	return addrs, nil
+	return nil
 }
 
 // Consumer group will default to Sarama if there is no value passed in
-func (ac *AppConfig) createKafkaConsumer(brokers []string, tc *tls.Config) (*cluster.Consumer, error) {
+func (ac *appConfig) createKafkaConsumer(topic string, consumerGroup string, brokers []string, tc *tls.Config) (*cluster.Consumer, error) {
 	config := cluster.NewConfig()
 	config.Net.TLS.Config = tc
 	config.Net.TLS.Enable = true
 	config.Group.PartitionStrategy = cluster.StrategyRoundRobin
-	config.ClientID = ac.ConsumerGroup
+	config.ClientID = consumerGroup
 	config.Consumer.Return.Errors = true
-	group := ac.ConsumerGroup
+	group := consumerGroup
 	if ac.Prefix != "" {
 		group = strings.Join([]string{ac.Prefix, group}, "")
 	}
-	consumer, err := cluster.NewConsumer(brokers, group, []string{consumerTopic}, config)
+	consumer, err := cluster.NewConsumer(brokers, group, []string{topic}, config)
 	if err != nil {
 		return nil, err
 	}
 	return consumer, nil
 }
 
-func (ac *AppConfig) createKafkaAsyncProducer(brokers []string, tc *tls.Config) (sarama.AsyncProducer, error) {
+func (ac *appConfig) createKafkaAsyncProducer(brokers []string, tc *tls.Config) (sarama.AsyncProducer, error) {
 	config := sarama.NewConfig()
 	config.Net.TLS.Config = tc
 	config.Net.TLS.Enable = true
@@ -133,7 +140,7 @@ func (ac *AppConfig) createKafkaAsyncProducer(brokers []string, tc *tls.Config) 
 	return producer, nil
 }
 
-func (ac *AppConfig) createKafkaSyncProducer(brokers []string, tc *tls.Config) (sarama.AsyncProducer, error) {
+func (ac *appConfig) createKafkaSyncProducer(brokers []string, tc *tls.Config) (sarama.SyncProducer, error) {
 	config := sarama.NewConfig()
 	config.Net.TLS.Config = tc
 	config.Net.TLS.Enable = true

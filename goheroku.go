3d5d9e21c1e8aefcab6d1dc7c71c064ea897e14f
Meredith Lind <meredith.lind@hope.edu>
2017-06-22 14:32:14 -0400

    Removes AppConfig and dependency for handling env
    This is the first of multiple commits to overhaul the package. The idea
    is to let the user set their own configs instead of using a default.
    This will be easier to use and be useful to more people.
    
    In this first step, the AppConfig, which was simply holding the env
    variables, is done away with and now the env vars are retrieved as needed
    using the standard OS package instead of a third party package.
    
    Basically, these are the barebones of what this package does. Next step
    will be to build up the functionality and add in functions to create
    consumers and producers.

--- goheroku.go
+++ goheroku.go
@@ -6,163 +6,67 @@ import (
 	"errors"
 	"github.com/Shopify/sarama"
 	cluster "github.com/bsm/sarama-cluster"
-	"github.com/joeshaw/envdecode"
-	"log"
 	"net/url"
+	"os"
 	"strings"
 )
-
-type appConfig struct {
-	URL           string `env:"KAFKA_URL,required"`
-	TrustedCert   string `env:"KAFKA_TRUSTED_CERT,required"`
-	ClientCertKey string `env:"KAFKA_CLIENT_CERT_KEY,required"`
-	ClientCert    string `env:"KAFKA_CLIENT_CERT,required"`
-	Prefix        string `env:"KAFKA_PREFIX"`
-	TLSConfig     *tls.Config
-	BrokerAddrs   []string
-}
-
-// This function requires that you specify the topic that the returned consumer
-// will consume from and the group that it will belong to.
-func NewConsumer(topic string, consumerGroup string) (*cluster.Consumer, error) {
-	ac, _ := setupConnection()
-	consumer, err := ac.createKafkaConsumer(topic, consumerGroup, ac.BrokerAddrs, ac.TLSConfig)
-	if err != nil {
-		return nil, err
-	}
-	return consumer, nil
-}
-
-// For move information about the difference between Async and Sync producers,
-// see the Sarama docs
-func NewAsyncProducer() (sarama.AsyncProducer, error) {
-	ac, _ := setupConnection()
-	producer, err := ac.createKafkaAsyncProducer(ac.BrokerAddrs, ac.TLSConfig)
-	if err != nil {
-		return nil, err
-	}
-	return producer, nil
-}
-
-// For move information about the difference between Async and Sync producers,
-// see the Sarama docs
-func NewSyncProducer() (sarama.SyncProducer, error) {
-	ac, _ := setupConnection()
-	producer, err := ac.createKafkaSyncProducer(ac.BrokerAddrs, ac.TLSConfig)
-	if err != nil {
-		return nil, err
-	}
-	return producer, nil
-}
 
 // To specify the topic or consumer group, the Kafka prefix needs
 // to appended to it. This function makes it possible without having
 // access to the app config.
 func AppendPrefixTo(str string) string {
-	ac := appConfig{}
-	err := envdecode.Decode(&ac)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if ac.Prefix != "" {
-		str = strings.Join([]string{ac.Prefix, str}, "")
+	prefix := os.Getenv("KAFKA_PREFIX")
+	if prefix != "" {
+		str = strings.Join([]string{prefix, str}, "")
 	}
 	return str
 }
 
-func setupConnection() (*appConfig, error) {
-	ac := appConfig{}
-	err := envdecode.Decode(&ac)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	err = ac.createTLSConfig()
-	if err != nil {
-		return nil, err
-	}
-	err = ac.brokerAddresses()
-	if err != nil {
-		return nil, err
-	}
-	return &ac, nil
-}
-
 // Create the TLS context, using the key and certificates provided.
-func (ac *appConfig) createTLSConfig() error {
-	roots := x509.NewCertPool()
-	ok := roots.AppendCertsFromPEM([]byte(ac.TrustedCert))
+func createTLSConfig() (*tls.Config, error) {
+	trustedCert, ok := os.LookupEnv("KAFKA_TRUSTED_CERT")
 	if !ok {
-		return errors.New("Unable to parse Root Cert. Please check your Heroku environment.")
+		return nil, errors.New("Kafka Trusted Certificate not found!")
+	}
+
+	clientCertKey, ok := os.LookupEnv("KAFKA_CLIENT_CERT_KEY")
+	if !ok {
+		return nil, errors.New("Kafka Client Certificate Key not found!")
+	}
+
+	clientCert, ok := os.LookupEnv("KAFKA_CLIENT_CERT")
+	if !ok {
+		return nil, errors.New("Kafka Client Certificate not found!")
+	}
+
+	roots := x509.NewCertPool()
+	ok = roots.AppendCertsFromPEM([]byte(trustedCert))
+	if !ok {
+		return nil, errors.New("Unable to parse Root Cert. Please check your Heroku environment.")
 	}
 	// Setup certs for Sarama
-	cert, err := tls.X509KeyPair([]byte(ac.ClientCert), []byte(ac.ClientCertKey))
+	cert, err := tls.X509KeyPair([]byte(clientCert), []byte(clientCertKey))
 	if err != nil {
-		return err
+		return nil, err
 	}
-	ac.TLSConfig = &tls.Config{
+	return &tls.Config{
 		Certificates:       []tls.Certificate{cert},
 		InsecureSkipVerify: true,
 		RootCAs:            roots,
-	}
-	return nil
+	}, nil
 }
 
 // Extract the host:port pairs from the Kafka URL(s)
-func (ac *appConfig) brokerAddresses() error {
-	urls := strings.Split(ac.URL, ",")
+func brokerAddresses() ([]string, error) {
+	URL := os.Getenv("KAFKA_URL")
+	urls := strings.Split(URL, ",")
 	addrs := make([]string, len(urls))
 	for i, v := range urls {
 		u, err := url.Parse(v)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		addrs[i] = u.Host
-		ac.BrokerAddrs = addrs
 	}
-	return nil
-}
-
-// Consumer group will default to Sarama if there is no value passed in
-func (ac *appConfig) createKafkaConsumer(topic string, consumerGroup string, brokers []string, tc *tls.Config) (*cluster.Consumer, error) {
-	config := cluster.NewConfig()
-	config.Net.TLS.Config = tc
-	config.Net.TLS.Enable = true
-	config.Group.PartitionStrategy = cluster.StrategyRoundRobin
-	config.ClientID = consumerGroup
-	config.Consumer.Return.Errors = true
-	group := consumerGroup
-	group = AppendPrefixTo(group)
-	topic = AppendPrefixTo(topic)
-	consumer, err := cluster.NewConsumer(brokers, group, []string{topic}, config)
-	if err != nil {
-		return nil, err
-	}
-	return consumer, nil
-}
-
-func (ac *appConfig) createKafkaAsyncProducer(brokers []string, tc *tls.Config) (sarama.AsyncProducer, error) {
-	config := sarama.NewConfig()
-	config.Net.TLS.Config = tc
-	config.Net.TLS.Enable = true
-	config.Producer.Return.Errors = true
-	config.Producer.RequiredAcks = sarama.WaitForAll // Default is WaitForLocal
-	producer, err := sarama.NewAsyncProducer(brokers, config)
-	if err != nil {
-		return nil, err
-	}
-	return producer, nil
-}
-
-func (ac *appConfig) createKafkaSyncProducer(brokers []string, tc *tls.Config) (sarama.SyncProducer, error) {
-	config := sarama.NewConfig()
-	config.Net.TLS.Config = tc
-	config.Net.TLS.Enable = true
-	config.Producer.Return.Errors = true
-	config.Producer.RequiredAcks = sarama.WaitForAll // Default is WaitForLocal
-	producer, err := sarama.NewSyncProducer(brokers, config)
-	if err != nil {
-		return nil, err
-	}
-	return producer, nil
+	return addrs, nil
 }
